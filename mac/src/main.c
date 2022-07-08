d2761c85c45a436946119e6bcffe595d5c445d5c
ksura12 <kevin.sura@outlook.de>
2022-07-08 14:18:50 +0200

    testing writing pixels to image

--- mac/src/main.c
+++ mac/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 09:40:10 by ksura             #+#    #+#             */
-/*   Updated: 2022/07/08 10:57:24 by ksura            ###   ########.fr       */
+/*   Updated: 2022/07/08 14:18:06 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,23 @@ int	main(int argc, char **argv)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
-	
+	// char	*relative_path = "./mandelbrot.xpm";
+	// int		img_width = 354;
+	// int		img_height = 337;
+
 	if (argc >= 1)
 	{
 		mlx = mlx_init();
 		mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "First Window");
 		img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
 		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-		my_mlx_pixel_put(&img, 0, 0, 0x00FF0000);
-		mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 		
+		// img.img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+		// ft_printf("%i\n", img.img);
+		ft_printf("%s\n", argv[1]);						
+		mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 		mlx_loop(mlx);
-		ft_printf("%s\n", argv[1]);
+		
 	}
 }
 
