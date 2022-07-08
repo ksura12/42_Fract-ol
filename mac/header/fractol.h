1b72ac9a0f454d40f848deaac56c4bd132354d7b
ksura12 <kevin.sura@outlook.de>
2022-07-08 09:20:47 +0200

    header

---
+++ mac/header/fractol.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 09:09:05 by ksura             #+#    #+#             */
+/*   Updated: 2022/07/08 09:18:46 by ksura            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "../mlx/mlx.h"
+# include "../libft/header/libftprintf.h"
+# include <math.h>
+# include <stdlib.h>
+
+#endif
