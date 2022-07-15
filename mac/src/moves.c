b566b8ba422194defbc4bb5cee25639146dadb9e
ksura12 <kevin.sura@outlook.de>
2022-07-15 10:38:35 +0200

    fisxing movedirection

--- mac/src/moves.c
+++ mac/src/moves.c
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 16:35:04 by ksura             #+#    #+#             */
-/*   Updated: 2022/07/15 10:16:54 by ksura            ###   ########.fr       */
+/*   Updated: 2022/07/15 10:27:30 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,15 @@ void move_right(t_data *data)
 void move_down(t_data *data)
 {
 	data->shift = 0.05;
-	data->min_i -= (data->max_i - data->min_i) * data->shift;
-	data->max_i -= (data->max_i - data->min_i) * data->shift;
+	data->min_i += (data->max_i - data->min_i) * data->shift;
+	data->max_i += (data->max_i - data->min_i) * data->shift;
 	return;
 }
 
 void move_up(t_data *data)
 {
 	data->shift = 0.05;
-	data->max_i += (data->max_i - data->min_i) * data->shift;
-	data->min_i += (data->max_i - data->min_i) * data->shift;
+	data->max_i -= (data->max_i - data->min_i) * data->shift;
+	data->min_i -= (data->max_i - data->min_i) * data->shift;
 	return;
 }
